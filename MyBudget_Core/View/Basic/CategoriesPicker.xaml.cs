b23fdab5a2e834f07e658e8901466497f64c9f0c
Sebastian Krupa <45374275+sebkrupa@users.noreply.github.com>
2020-05-15 22:52:37 +0200

    charts added

---
+++ MyBudget_Core/View/Basic/CategoriesPicker.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace MyBudget_Core.View.Basic
+{
+    /// <summary>
+    /// Interaction logic for CategoriesPicker.xaml
+    /// </summary>
+    public partial class CategoriesPicker : UserControl
+    {
+        public CategoriesPicker()
+        {
+            InitializeComponent();
+            cbxCategory.ItemsSource = new MyBudgetAPI.Read.Category().GetAll().OrderBy(x => x.name);
+            cbxCategory.SelectionChanged += (s, e) => SelectCategory();
+        }
+
+        public virtual void SelectCategory()
+        {
+            if (cbxCategory.SelectedItem != null)
+            {
+                cbxSubCategory.ItemsSource = null;
+                var list = new MyBudgetAPI.Read.SubCategory().GetAll().Where(x =>
+                x.categoryId == ((MyBudgetAPI.Model.Category)cbxCategory.SelectedItem).id).ToList();
+                cbxSubCategory.ItemsSource = list;
+            }
+        }
+    }
+}
---
+++ MyBudget_Core/View/Statistics/Charting.xaml.cs
@@ -0,0 +1,42 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace MyBudget_Core.View.Statistics
+{
+    /// <summary>
+    /// Interaction logic for Charting.xaml
+    /// </summary>
+    public partial class Charting : UserControl
+    {
+        public SeriesCollection mySeries { get; set; } = new SeriesCollection();
+        public List<string> labelsX { get; set; } = new string[] { "sty", "lut", "mar", "kwi", "maj", "cze", "lip", "sie", "wrz", "paz", "lis", "gru" }.ToList();
+        public Charting()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+            
+        public void SetXLabels(string[] labels) => chart.AxisX[0].Labels = labels.ToList();
+        public void AddToChart(string title, double[] values)
+        {
+            var series = new LiveCharts.Wpf.LineSeries();
+            series.Title = title;
+            ChartValues<double> chartValues = new ChartValues<double>();
+            foreach (var c in values)
+                chartValues.Add(c);
+            series.Values = chartValues;
+            mySeries.Add(series);
+        }
+
+        public void RemoveFromChart(string title)
+        {
+            var toRemove = mySeries.Where(x => x.Title == title).FirstOrDefault();
+            mySeries.Remove(toRemove);
+        }
+    }
+}
